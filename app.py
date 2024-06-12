c57e50956e11fed013dbbd01decc57fdd4c4a062
romacw1484 <wromac@fordham.edu>
2024-06-12 12:45:33 -0400

    Updated chat route to point to index.html

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-# app.py 
+# app.py  
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_socketio import SocketIO, send
-from models import db, User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1fe162a579d7de0e7d65a09dfec35e0f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-db.init_app(app)
+db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-socketio = SocketIO(app)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -46,7 +47,7 @@ def signup():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        hashed_password = generate_password_hash(password, method='sha256')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
@@ -62,14 +63,9 @@ def logout():
 @app.route('/chat')
 @login_required
 def chat():
-    return render_template('index.html')
-
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
+    return render_template('index.html')  # Ensure this points to index.html
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+    app.run(debug=True)
